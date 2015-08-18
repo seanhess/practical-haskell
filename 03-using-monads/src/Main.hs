b9ab73f10e6fa9533d43d07f6669432c5b7d7374
Sean Clark Hess <seanhess@gmail.com>
2015-08-18 14:22:43 -0600

    monads and rest api

---
+++ 03-using-monads/src/Main.hs
@@ -0,0 +1,6 @@
+main = do
+  let action = putStrLn "Hello World"
+  action
+  action
+  action
+  return ()
---
+++ 04-rest-api/src/Main.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+import Data.Monoid ((<>))
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics
+import Web.Scotty
+
+data User = User { userId :: Int, userName :: String } deriving (Generic)
+instance ToJSON User
+instance FromJSON User
+
+bob :: User
+bob = User { userId = 1, userName = "bob" }
+
+jenny :: User
+jenny = User { userId = 2, userName = "jenny" }
+
+allUsers :: [User]
+allUsers = [bob, jenny]
+
+matchesId :: Int -> User -> Bool
+matchesId id user = userId user == id
+
+main = do
+  putStrLn "Starting Server..."
+  scotty 3000 $ do
+    get "/hello/:name" $ do
+      name <- param "name"
+      text ("hello " <> name <> "!")
+
+    get "/users" $ do
+      json allUsers
+
+    get "/users/:id" $ do
+      id <- param "id"
+      json (filter (matchesId id) allUsers)
